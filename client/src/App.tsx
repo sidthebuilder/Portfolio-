27d78cfba00a55907f0afa1930a48d7d90b145ff
sidthebuilder <sidthebuilder@users.noreply.github.com>
2026-01-31 12:23:51 +0530

    Fix: Add base path to Wouter router for GitHub Pages deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,10 +7,12 @@ import TerminalPage from "@/pages/Terminal";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={TerminalPage} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/Portfolio-">
+      <Switch>
+        <Route path="/" component={TerminalPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
